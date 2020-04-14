5dac04c44c8c2b517adab8ebd762157d41ae56a2
msullivancm <sullivan@tibrasil.net>
2020-04-13 21:02:39 -0300

    protheus funcional acessndo dbaccess e banco de dados

--- appserver/init.sh
+++
@@ -1,9 +0,0 @@
-export TOTVS_HOME=/totvs/protheus/bin/appserver
-export TOTVS_BIN=$TOTVS_HOME/appsrvlinux
-PATH=/usr/sbin:$PATH; export PATH
-ulimit -n 65536
-ulimit -s 1024
-ulimit -c unlimited
-ulimit -f unlimited
-ulimit -v unlimited
-
