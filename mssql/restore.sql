678201f797c92a8fb7f784aefe53ac25106594dc
msullivancm <sullivan@tibrasil.net>
2020-04-13 22:47:14 -0300

    Modificação do MSSQL para que, caso o banco não exista, crie com as tabelas do banco de TESTE.

--- mssql/restore.sql
+++ mssql/restore.sql
@@ -1,9 +1,11 @@
 --ALTER DATABASE [dadosadv] SET OFFLINE WITH ROLLBACK IMMEDIATE
-USE [master] RESTORE DATABASE [dadosadv] FROM  DISK = '/tmp/p12127.bak' WITH  FILE = 1,  
+USE [master] 
+RESTORE DATABASE [dadosadv] FROM  DISK = '/tmp/p12127.bak' WITH  FILE = 1,  
     --MOVE N'P12127_Data' TO N'/var/opt/mssql/data/dadosadv.mdf',  
     --MOVE N'P12127_Log' TO N'/var/opt/mssql/data/dadosadv_log.ldf',  
     NOUNLOAD,  REPLACE,  STATS = 5
-USE [dadosadv] exec sp_changedbowner siga
+USE [dadosadv] 
+exec sp_changedbowner siga
 --USE [master] ALTER DATABASE [dadosadv] SET RECOVERY SIMPLE WITH NO_WAIT
 --USE [dadosadv] DBCC SHRINKFILE (N'DADOSLLXMR_Log' , 0, TRUNCATEONLY)
 
